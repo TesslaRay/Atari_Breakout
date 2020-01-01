ed73a490ae86585995a19a8ae1422f1cf722a2d4
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2020-01-01 14:28:26 -0300

    one block working

--- breakout.py
+++ breakout.py
@@ -14,18 +14,18 @@ paddle = turtle.Turtle()
 paddle.speed(0)
 paddle.shape("square")
 paddle.color("blue")
-paddle.shapesize(stretch_wid=1, stretch_len=7)
+paddle.shapesize(stretch_wid=1, stretch_len=5)
 paddle.penup()
 paddle.goto(0 , -260) 
 
 # Ball 
-ball_move = 2
+ball_move = 4
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("square")
+ball.shape("circle")
 ball.color("blue")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0,-240)
 ball.dx = ball_move
 ball.dy = ball_move
 
@@ -38,15 +38,27 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Points: 0", align="center", font=("Courier", 24,"normal"))
 
-# # Functions
+# Score
+score = 0
+
+# Structure
+blue = turtle.Turtle()
+blue.speed(0)
+blue.shape("square")
+blue.color("blue")
+blue.shapesize(stretch_wid=1, stretch_len=5)
+blue.penup()
+blue.goto(0, 50)
+
+# Functions
 def paddle_right():
     x = paddle.xcor()
-    x += 20
+    x += 30
     paddle.setx(x)
 
 def paddle_left():
     x = paddle.xcor()
-    x -= 20
+    x -= 30
     paddle.setx(x)
 
 # Keyboard binding
@@ -63,8 +75,8 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Border checking
-    if ball.ycor() > 290:
-        ball.sety(290)
+    if ball.ycor() > 270:
+        ball.sety(270)
         ball.dy *= -1
 
     if ball.ycor() < -290:
@@ -84,3 +96,11 @@ while True:
     if (ball.ycor() < -240 and ball.ycor() > -250) and (ball.xcor() < paddle.xcor() + 50 and ball.xcor() > paddle.xcor() - 50):
         ball.sety(-240)
         ball.dy *= -1
+
+    # Ball and structure collisions
+    if (ball.ycor() > 40 and ball.ycor() < 50) and (ball.xcor() < blue.xcor() +  50 and ball.xcor() > blue.xcor() - 50) and blue.isvisible():
+        ball.dy *= -1
+        blue.hideturtle()
+        score += 1
+        pen.clear()
+        pen.write("Points: {}".format(score), align="center", font=("Courier", 24,"normal"))
